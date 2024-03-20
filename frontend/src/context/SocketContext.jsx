fff2ccceb909502cb68d6cddcf5bf9e31964941b
dantejayaram <dantejayaram@gmail.com>
2024-03-21 00:31:25 +0530

    Socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socketInstance = io("http://localhost:5000", {
+      const socketInstance = io("https://chat-app-test-u7zj.onrender.com", {
         query: {
           userId: authUser._id,
         },
