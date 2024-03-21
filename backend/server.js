77b57044989cf36781f46675fc9f3ccbae252bd5
dantejayaram <dantejayaram@gmail.com>
2024-03-21 22:27:52 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,48 @@
 
+
+
+
+
+
+
+
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from  "./routes/auth.routes.js";
+import messageRoutes from  "./routes/message.routes.js";
+import userRoutes from  "./routes/user.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server, io } from './socket/socket.js'; // Importing app, server, and io from socket.js
+const PORT = process.env.PORT || 8000;
+const __dirname = path.resolve();
+dotenv.config();
+
+app.use(cookieParser());
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+
+server.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
+
+
+
+
+
+/*
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
@@ -48,7 +92,7 @@ server.listen(PORT, () => {
   console.log(`Server Running on port ${PORT}`);
 });
 
-
+*/
 
 
 
